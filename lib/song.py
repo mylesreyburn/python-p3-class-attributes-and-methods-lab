16489bb389ac1ab04f56de69ecd76932354b05a4
Myles Reyburn <mylesreyburn@gmail.com>
2023-03-21 15:28:09 -0600

    done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,33 @@
 class Song:
-    pass
+    
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre) -> None:
+        self.increase_song_count()
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_artist(artist)
+        self.add_genre(genre)
+
+    @classmethod
+    def increase_song_count(cls):
+        cls.count += 1
+        
+    @classmethod
+    def add_artist(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+            cls.artist_count[artist] = 0
+        cls.artist_count[artist] += 1
+
+    @classmethod
+    def add_genre(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+            cls.genre_count[genre] = 0
+        cls.genre_count[genre] += 1
